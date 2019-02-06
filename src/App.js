ef0b5a1e6773618d4869f47933301b701fba7d9e
Surendran <ranjith.surendran@accenture.com>
2019-02-06 14:10:18 +0100

    Types of Test

--- src/App.js
+++ src/App.js
@@ -701,6 +701,16 @@ class App extends Component {
             <div>
               <div className="content-wrapper">
                 <div className="content-header">
+                  <h1>Types of Test</h1>
+                </div>
+                <div className="content-description">
+                  <img src="./images/ToT.jpg" alt="logo" />
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="content-wrapper">
+                <div className="content-header">
                   <h1>TDD</h1>
                 </div>
                 <div className="content-description">
