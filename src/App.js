c76f717ee771ab304a01be90981229940e0002bd
willy.noel.nzefameni <nzefa_noel@yahoo.fr>
2019-02-06 10:34:01 +0100

    update reporting a Bug

--- src/App.js
+++ src/App.js
@@ -827,6 +827,17 @@ class App extends Component {
                 </div>
               </div>
             </div>
+			
+			<div>
+              <div className="content-wrapper">
+                <div className="content-header">
+                  <h1>Reporting a bug</h1>
+                </div>
+                <div className="content-description">
+                  <img src="./images/Reporting_a_Bug.jpg" alt="logo" />
+                </div>
+              </div>
+            </div>
 
 
         </div>
